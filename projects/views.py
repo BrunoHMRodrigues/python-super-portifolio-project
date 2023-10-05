95f67e0418913070b162beb5311e8cb00abe884d
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2023-10-04 21:37:27 -0300

    Feature: Requisite 04 - customize viewsets, missing certificate in html

--- projects/views.py
+++ projects/views.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import (
     IsAuthenticated, IsAuthenticatedOrReadOnly)
+from django.shortcuts import render, get_object_or_404
 # from django.shortcuts import render, get_object_or_404
 from .models import Profile, Project
 from .serializers import ProfileSerializer, ProjectSerializer
@@ -12,11 +13,26 @@ class ProfileViewSet(viewsets.ModelViewSet):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
     authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
-    def get_permissions(self):
-        if self.action == 'list':
-            return [IsAuthenticatedOrReadOnly()]
-        return [IsAuthenticated()]
+    # def get_permissions(self):
+    #     if self.action == 'list':
+    #         return [IsAuthenticatedOrReadOnly()]
+    #     return [IsAuthenticated()]
+    
+    def retrieve(self, request, *args, **kwargs):
+        if request.method == 'GET':
+            profile = get_object_or_404(Profile, pk=kwargs['pk'])
+            projects = Project.objects.filter(profile=profile)
+
+            context = {
+                'profile': profile,
+                'projects': projects,
+            }
+
+            return render(request, 'profile_detail.html', context)
+
+        return super().retrieve(request, *args, **kwargs)
 
 
 class ProjectViewSet(viewsets.ModelViewSet):
